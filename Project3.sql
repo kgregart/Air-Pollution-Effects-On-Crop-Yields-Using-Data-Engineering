ce6e2b26fb9f17ae5278531ae20f62c0a114744a
kaypea87 <153476582+kaypea87@users.noreply.github.com>
2024-04-17 11:10:38 -0400

    Add files via upload

--- Project3.sql
+++ Project3.sql
@@ -88,24 +88,57 @@ ORDER BY Year;
 
 SELECT * FROM annual_aqi_by_county;
 
-CREATE TABLE farm_count (
+CREATE TABLE farm_count_2 (
+	"Year" INTEGER,
 	"State Code" VARCHAR(50),
     "County Code" VARCHAR(50),
 	"State" VARCHAR(255),
     "County" VARCHAR(255),
 	"State County Code" VARCHAR(50),
 	"Farm Count" VARCHAR(255),
-	PRIMARY KEY ("State", "County")
+	PRIMARY KEY ("State", "County","Year")
 );
 
-SELECT * FROM farm_count;
+SELECT * FROM farm_count_2;
+
 
 ALTER TABLE annual_conc_by_monitor_3
 ADD FOREIGN KEY ("State Name", "County Name", "Year") REFERENCES annual_aqi_by_county(State, County, Year);
 
-ALTER TABLE annual_conc_by_monitor_3 ADD PRIMARY KEY ();
+--comment for now: ALTER TABLE annual_conc_by_monitor_3 ADD PRIMARY KEY ();
 
-SELECT FROM annual_conc_by_monitor_3
-WHERE ("State Name" = 'Ohio',  "County Name" = 'Adams', "Year" = '1997');
+SELECT * FROM annual_conc_by_monitor_3
+WHERE "State Name" = 'Ohio' AND  "County Name" = 'Adams' AND "Year" = '1997';
 
+DELETE FROM annual_conc_by_monitor_3
+WHERE "State Name" = 'Ohio' AND  "County Name" = 'Adams' AND "Year" = '1997';
 
+--Deleted all rows that had State Name = Ohio AND County Name = Adams as this does not exist in the aqi by county data
+
+SELECT * FROM annual_conc_by_monitor_3
+WHERE "State Name" = 'Ohio' AND  "County Name" = 'Adams';
+
+DELETE FROM annual_conc_by_monitor_3
+WHERE "State Name" = 'Ohio' AND  "County Name" = 'Adams';
+
+--created a new combined table from a query
+CREATE TABLE combine_aqu_conc AS (
+SELECT * FROM annual_conc_by_monitor_3 JOIN annual_aqi_by_county 
+ON annual_conc_by_monitor_3."State Name" =  annual_aqi_by_county.State 
+AND annual_conc_by_monitor_3."County Name" =  annual_aqi_by_county.County
+AND annual_conc_by_monitor_3."Year" =  annual_aqi_by_county.Year);
+
+SELECT * FROM combine_aqu_conc;
+
+--trying to add farm data, but this only outputs limited data
+SELECT DISTINCT Year FROM combine_aqu_conc JOIN farm_count_2
+ON combine_aqu_conc."Year" =  farm_count_2."Year"
+AND combine_aqu_conc."State Name" =  farm_count_2."State"
+AND combine_aqu_conc."County Name" =  farm_count_2."County";
+
+--
+SELECT 
+DISTINCT "Year"
+FROM 
+combine_aqu_conc
+ORDER BY "Year";
